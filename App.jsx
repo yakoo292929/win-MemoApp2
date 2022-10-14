6a13a5b7865eb8ee77a59cb28205b38b925d35fc
yakoo292929 <yakoo292929@gmail.com>
2022-10-14 21:20:58 +0900

    Create LoginScreen & SignUpScreen

--- App.jsx
+++ App.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
     // <MemoEditScreen />
-    <MemoCreateScreen />
+    // <MemoCreateScreen />
+    // <LoginScreen />
+    <SignUpScreen />
   );
 }
