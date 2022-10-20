de763bfaa05e04bf6af30c45c753819c9eb5f74a
yakoo292929 <yakoo292929@gmail.com>
2022-10-21 06:14:34 +0900

    Fetch data form firebase on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -4,6 +4,8 @@ import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { shape, string, instanceOf, arrayOf } from 'prop-types';
 
+import { dateToString } from "../utils";
+
 export default function MemoList(props) {
   const { memos } = props;
   const navigation = useNavigation();
@@ -12,11 +14,11 @@ export default function MemoList(props) {
     return(
       <TouchableOpacity
         style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDetail'); } }
+        onPress={() => { navigation.navigate('MemoDetail',{ id: item.id }); } }
       >
         <View>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+          <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
