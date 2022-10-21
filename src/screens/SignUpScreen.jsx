7f7b15b2f06c931636483d91d6b58c0b84901290
yakoo292929 <yakoo292929@gmail.com>
2022-10-22 08:53:14 +0900

    Improve ux

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -3,13 +3,18 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert } from "reac
 import firebase from "firebase";
 
 import Button from "../components/Button";
+import Loading from "../components/Loading";
+import { translateErrors } from "../utils";
+
 
 export default function SignUpScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   function handlePress() {
+    setLoading(true);
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       const { user } = userCredential;
@@ -21,12 +26,17 @@ export default function SignUpScreen(props) {
     })
     .catch((error) => {
       console.log(error.code, error.message);
-      Alert.alert(error.code);
+      const errorMsg = translateErrors(error.code);
+      Alert.alert(errorMsg.title, errorMsg.description);
+    })
+    .then(() => {
+      setLoading(false);
     });
   }
 
   return (
     <View style={styles.container}>
+      <Loading isloading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Sign Up</Text>
         <TextInput
@@ -57,7 +67,7 @@ export default function SignUpScreen(props) {
             onPress={() => {
               navigation.reset({
                 index: 0,
-                routes: [{ name: 'Login' }],
+                routes: [{ name: 'LogIn' }],
               });
             }}
           >
